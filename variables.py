c796dcbf21e48acea6f615a0b9db02fd8305f57c
winternFx <culerarjun@gmail.com>
2026-05-04 17:38:00 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "alice"
+age = 20
+height = 1.57
+is_student = True
+
+
+x= input("Enter name")
+if x == name :
+    print ("The users name is" ,name)    
+    print (name , " is ", age , " years old.")
+else    :
+
+    print("Invalid user")
