2c3ffb00bd6b8fda7b42e8d75d0cb9d9bb0e89ae
winternFx <culerarjun@gmail.com>
2026-05-20 01:36:39 +0530

    typecast

---
+++ person.py
@@ -0,0 +1,10 @@
+age = 21
+salary = 12000
+height = 170.5 
+name = input("What is your name?")
+print("Hello", name, "Welcome to python")
+ages = "21"
+
+print(name)
+print ("Anna")
+print ("the age of ", name , "is ", int(ages))
