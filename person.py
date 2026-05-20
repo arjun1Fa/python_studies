7de8d555028682cc15d9f087f90fec891cd627c9
winternFx <culerarjun@gmail.com>
2026-05-21 01:00:42 +0530

    feat: implement dynamic user age input and loop-based exponentiation calculations

--- person.py
+++ person.py
@@ -1,10 +1,38 @@
+
+
 age = 21
 salary = 12000
 height = 170.5 
-name = input("What is your name?")
+name = input("What is your name?\n")
 print("Hello", name, "Welcome to python")
-ages = "21"
+age = int(input("How old are you?\n"))
+print("\nSo you are ", name, age, " years old")
+
 
 print(name)
-print ("Anna")
-print ("the age of ", name , "is ", int(ages))
+print ("Anna") 
+print ("the age of ", name , "is ", int(ages))*/
+
+
+
+n = int(input("Enter number to square"))
+square = 1
+for i in range (5):
+    square *= n
+
+
+print("Value of the square function is " , square)
+
+m = int(input("Enter number"))
+expo=int(input("enter the value of the expo"))
+squar = 1
+for i in range (expo):
+    squar = squar * m
+
+
+print("Value of the square function is " , squar)
+
+a=int (7)
+b= int(2)
+
+print(a/b)
