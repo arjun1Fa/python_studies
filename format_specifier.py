aff91d44625dcbeeefe7dbdffdaba4e00facb4ab
winternFx <culerarjun@gmail.com>
2026-05-21 21:19:44 +0530

    feat: add string alignment padding examples to format specifier demonstration

--- format_specifier.py
+++ format_specifier.py
@@ -1,3 +1,4 @@
+'''
 import math
 
 print(f"{math.pi:.2f}")
@@ -5,3 +6,11 @@ print(f"{math.pi:.5}")
 print(f"{math.pi:.5f}")
 print(f"{math.pi:.1f}")
 print(f"{math.pi:.1}")
+
+'''
+
+name = "Ana"
+print(f"|{name:10}|")   # |Ana       |   (7 spaces after)
+print(f"|{name:>10}|")  # |> for right align (default)|   explicit
+print(f"|{name:<10}|")  # |Ana       |   left align
+print(f"|{name:^10}|")  # |   Ana    |   center
