276f890e9524621171bb93b08ff2726e6d8709e7
winternFx <culerarjun@gmail.com>
2026-05-21 21:35:23 +0530

    while loops

--- while.py
+++ while.py
@@ -4,7 +4,12 @@ i = 0
 while i< 20 :
     if (i % 2) == 0 :
         print(i," is an even number\n")
+    elif i == 17 :
+        break
     else :
         print("\t\t\t\t",i, " is an odd number \t\t")
+   
+        
+
     
     i += 1
