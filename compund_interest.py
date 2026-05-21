1ef106e7eda94fbae7332146b5977dadfa1e87dc
winternFx <culerarjun@gmail.com>
2026-05-21 22:26:56 +0530

    while loop

---
+++ compund_interest.py
@@ -0,0 +1,19 @@
+import math
+
+while True: 
+    p = int(input("Enter initial innvestment: "))
+    r = float(input("Enter the interest rate"))
+    n = int(input("Enter the number of times interest is compunded "))
+    t = int(input("Enter the number of years of investment: "))
+    r = r / 100
+
+    a = p * (1 + r/n)**(n*t)
+    print(f"\n\n Your annual interest will be :{a:.2f}" )
+
+    choice = input("DO you wish to calculate more?.. yes or no").lower()
+
+    if choice != "yes" and choice != "y" and choice != "ok":
+        break
+    else :
+        continue
+
